7e2897b7db32bf1b46187c54e3d3b3ddf9923d54
Richard Roy <richard.alex.roy@gmail.com>
2025-07-07 14:25:50 -0400

    fixing path

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    basePath: isProd ? '/portfolio' : '',
-    assetPrefix: isProd ? '/portfolio/' : '',
+    basePath: isProd ? '/Portfolio' : '',
+    assetPrefix: isProd ? '/Portfolio/' : '',
     trailingSlash: true,
     output: 'export',
 };
